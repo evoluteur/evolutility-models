8f92f8a45b28a9413078fa70f22939b7e10e691d
evoluteur <ogiulieri@gmail.com>
2019-07-14 14:55:12 -0700

    Added sample data.

---
+++ data/all_data.js
@@ -0,0 +1,32 @@
+/*
+  ___         _      _   _ _ _ _         
+ | __|_ _____| |_  _| |_(_) (_) |_ _  _  
+ | _|\ V / _ \ | || |  _| | | |  _| || | 
+ |___|\_/\___/_|\_,_|\__|_|_|_|\__|\_, | 
+                                   |__/  
+https://github.com/evoluteur/evolutility-models
+*/
+
+module.exports = {
+
+	// - Personal information Manager
+	todo: require('./pim/todo-data'),
+	contact: require('./pim/contact-data'),
+	comics: require('./pim/comics-data'),
+	winecellar: require('./pim/winecellar-data'),
+	winetasting: require('./pim/winetasting-data'),
+	restaurant: require('./pim/restaurant-data'),
+	
+	// - Music
+	album: require('./music/album-data'),
+	artist: require('./music/artist-data'),
+	track: require('./music/track-data'),
+
+	// - Test
+	test: require('./test/test-data'),
+
+	// - Designer
+	world: require('./designer/world-data'),
+	object: require('./designer/object-data'),
+	field: require('./designer/field-data'),
+}
---
+++ data/music/artist-data.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Snatam Kaur",
+        photo: "music/snatam_kaur.jpg",
+        url: 'https://www.snatamkaur.com/',
+        description: 'Through inspirational concerts, workshops, immersion courses and retreats, the much-beloved devotional singer and Grammy nominated recording artist Snatam Kaur shares the power of Sikh sacred mantras with all the world. Possessed of an incandescently luminous voice and a deep knowledge of Kundalini yoga, her concerts and events are joyous occasions and a source of solace for our troubled times.',
+    },
+    {
+        id: 2,
+        name: "Ashana",
+        photo: "music/ashana.jpg",
+        url: 'https://www.soundofashana.com/',
+        description: 'Weaving together soaring, angelic vocals and the celestial sound of crystal singing bowls, Ashana is one of the premiere healing music artists and New Age vocalists of our time. In fact, Ashana’s original sound is the forefront of an entirely new genre of healing music. This exquisitely crafted, luminous sonic tapestry soothes and opens the heart, enfolding the listener in a transcendent experience of the Divine and a direct experience of the Soul. This stunningly beautiful alchemy has been hailed by listeners all over the world as “breathtaking” and “a Gift from Heaven.”',
+    },
+    {
+        id: 3,
+        name: "Sade",
+        photo: "music/sade.jpg",
+        url: 'https://www.sade.com/',
+    },
+    {
+        id: 4,
+        name: "Moby",
+        photo: "music/moby.jpg",
+        url: 'http://moby.com',
+    },
+    {
+        id: 5,
+        name: "Naid",
+        photo: "music/naid-varanasi.jpg",
+        url: '',
+    },
+]
